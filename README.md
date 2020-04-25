05014b689df30cd89e28b6ef9d2ee7580c8c7519
Ellen <x.cheng@EllendeMacBook-Pro.local>
2020-04-25 12:01:04 -0500

    Traning

--- README.md
+++
@@ -1,2 +0,0 @@
-# Data_Mining_Project
-It is developed based on https://github.com/srihari-humbarwadi/DeepLabV3_Plus-Tensorflow2.0.git
--- train.py
+++ train.py
@@ -1,43 +1,106 @@
-from glob import glob
 import tensorflow as tf
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
 from deeplab import DeepLabV3Plus
-import data_generator
 
 
-batch_size = 24
+batch_size = 1
 H, W = 512, 512
 num_classes = 21
 
-train_dataset = data_generator.Dataset(
-          dataset_name='pascal_voc_seg',
-          split_name='train',
-          dataset_dir='./tfrecord/train-00000-of-00001.tfrecord',
-          batch_size=batch_size,
-          crop_size=[384, 384],
-          min_scale_factor=0.5,
-          max_scale_factor=2,
-          scale_factor_step_size=0.25,
-          model_variant='resnet_v1_50',
-          num_readers=4,
-          is_training=True,
-          should_shuffle=True,
-          should_repeat=True)
 
-val_dataset = data_generator.Dataset(
-          dataset_name='pascal_voc_seg',
-          split_name='val',
-          dataset_dir='./tfrecord/val-00000-of-00001.tfrecord',
-          batch_size=batch_size,
-          crop_size=[384, 384],
-          min_scale_factor=0.5,
-          max_scale_factor=2,
-          scale_factor_step_size=0.25,
-          model_variant='resnet_v1_50',
-          num_readers=4,
-          is_training=True,
-          should_shuffle=True,
-          should_repeat=True)
+def get_image(img, img_height=800, img_width=1600, mask=False, flip=0):
+    if not mask:
+        img = tf.cast(img, dtype=tf.float32)
+        img = tf.image.resize(images=img, size=[img_height, img_width])
+        img = tf.image.random_brightness(img, max_delta=50.)
+        img = tf.image.random_saturation(img, lower=0.5, upper=1.5)
+        img = tf.image.random_hue(img, max_delta=0.2)
+        img = tf.image.random_contrast(img, lower=0.5, upper=1.5)
+        img = tf.clip_by_value(img, 0, 255)
+        img = tf.case([
+            (tf.greater(flip, 0), lambda: tf.image.flip_left_right(img))
+        ], default=lambda: img)
+        img = img[:, :, ::-1] - tf.constant([103.939, 116.779, 123.68])
+    else:
+        img = tf.cast(tf.image.resize(images=img, size=[img_height, img_width]), dtype=tf.uint8)
+        img = tf.case([
+            (tf.greater(flip, 0), lambda: tf.image.flip_left_right(img))
+        ], default=lambda: img)
+    return img
+
+
+def random_crop(image, mask, H=512, W=512):
+    image_dims = image.shape
+    offset_h = tf.random.uniform(
+        shape=(1,), maxval=image_dims[0] - H, dtype=tf.int32)[0]
+    offset_w = tf.random.uniform(
+        shape=(1,), maxval=image_dims[1] - W, dtype=tf.int32)[0]
+
+    image = tf.image.crop_to_bounding_box(image,
+                                          offset_height=offset_h,
+                                          offset_width=offset_w,
+                                          target_height=H,
+                                          target_width=W)
+    mask = tf.image.crop_to_bounding_box(mask,
+                                         offset_height=offset_h,
+                                         offset_width=offset_w,
+                                         target_height=H,
+                                         target_width=W)
+    return image, mask
+
+
+def load_data(example_proto, H=512, W=512):
+    def _decode_image(content, channels):
+        return tf.cond(
+            tf.image.is_jpeg(content),
+            lambda: tf.image.decode_jpeg(content, channels),
+            lambda: tf.image.decode_png(content, channels))
+
+    features = {
+        'image/encoded':
+            tf.io.FixedLenFeature((), tf.string, default_value=''),
+        'image/filename':
+            tf.io.FixedLenFeature((), tf.string, default_value=''),
+        'image/format':
+            tf.io.FixedLenFeature((), tf.string, default_value='jpeg'),
+        'image/height':
+            tf.io.FixedLenFeature((), tf.int64, default_value=0),
+        'image/width':
+            tf.io.FixedLenFeature((), tf.int64, default_value=0),
+        'image/segmentation/class/encoded':
+            tf.io.FixedLenFeature((), tf.string, default_value=''),
+        'image/segmentation/class/format':
+            tf.io.FixedLenFeature((), tf.string, default_value='png'),
+    }
+
+    parsed_features = tf.io.parse_single_example(example_proto, features)
+
+    image = _decode_image(parsed_features['image/encoded'], channels=3)
+    label = _decode_image(parsed_features['image/segmentation/class/encoded'], channels=1)
+
+    flip = tf.random.uniform(
+        shape=[1, ], minval=0, maxval=2, dtype=tf.int32)[0]
+    image, mask = get_image(image, flip=flip), get_image(label, mask=True, flip=flip)
+    image, mask = random_crop(image, mask, H=H, W=W)
+    return image, mask
+train_dataset = tf.data.TFRecordDataset('dataset/tfrecord/train-00000-of-00001.tfrecord')
+train_dataset = train_dataset.apply(
+    tf.data.experimental.map_and_batch(map_func=load_data,
+                                       batch_size=batch_size,
+                                       num_parallel_calls=tf.data.experimental.AUTOTUNE,
+                                       drop_remainder=True))
+train_dataset = train_dataset.repeat()
+train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+print(train_dataset)
+
+val_dataset = tf.data.TFRecordDataset('dataset/tfrecord/val-00000-of-00001.tfrecord')
+val_dataset = val_dataset.apply(
+    tf.data.experimental.map_and_batch(map_func=load_data,
+                                       batch_size=batch_size,
+                                       num_parallel_calls=tf.data.experimental.AUTOTUNE,
+                                       drop_remainder=True))
+val_dataset = val_dataset.repeat()
+val_dataset = val_dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
 
 loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
@@ -61,11 +124,11 @@ mc = ModelCheckpoint(mode='min', filepath='top_weights.h5',
                      save_best_only='True',
                      save_weights_only='True', verbose=1)
 callbacks = [mc, tb]
-
-print(type(train_dataset))
 model.fit(train_dataset,
           steps_per_epoch=10582 // batch_size,
           epochs=300,
           validation_data=val_dataset,
           validation_steps=1449 // batch_size,
           callbacks=callbacks)
+
+model.save('my_model.h5')
